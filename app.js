d01df7004ab6ece837a6cc0877174f97adff256a
Amit Kumar <themit@gmail.com>
2011-06-28 10:32:17 -0400

    committing all to prepare for push to ec2

--- app.js
+++ app.js
@@ -4,10 +4,37 @@
  */
 
 var express = require('express');
-
+var http = require('http');
 var app = module.exports = express.createServer();
+//var net = require('net');
+var port = (process.env.VMC_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
 
+//tcpGuests = [];
 // Configuration
+if(process.env.VCAP_SERVICES){
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  var mongo = env['mongodb-1.8'][0]['credentials'];
+}
+else{
+  var mongo = {"hostname":"localhost","port":27017,"username":"",
+    "password":"","name":"","db":"db"}
+}
+
+var generate_mongo_url = function(obj){
+  obj.hostname = (obj.hostname || 'localhost');
+  obj.port = (obj.port || 27017);
+  obj.db = (obj.db || 'test');
+
+  if(obj.username && obj.password){
+    return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+  else{
+    return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+}
+
+var mongourl = generate_mongo_url(mongo);
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -26,18 +53,102 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
+
+// Methods
+var record_visit = function(req, res){
+  /* Connect to the DB and auth */
+  require('mongodb').connect(mongourl, function(err, conn){
+    conn.collection('ips', function(err, coll){
+      /* Simple object to insert: ip address and date */
+      object_to_insert = { 'ip': req.connection.remoteAddress, 'ts': new Date() };
+
+      /* Insert the object then print in response */
+      /* Note the _id has been created */
+      coll.insert( object_to_insert, {safe:true}, function(err){
+        if(err) { console.log(err.stack); }
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(JSON.stringify(object_to_insert));
+        res.end('\n');
+      });
+    });
+  });
+}
+
+var print_visits = function(req, res){
+  /* Connect to the DB and auth */
+  require('mongodb').connect(mongourl, function(err, conn){
+    conn.collection('ips', function(err, coll){
+      coll.find({}, {limit:10, sort:[['_id','desc']]}, function(err, cursor){
+        cursor.toArray(function(err, items){
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          for(i=0; i<items.length;i++){
+            res.write(JSON.stringify(items[i]) + "\n");
+          }
+          res.end();
+        });
+      });
+    });
+  });
+}
+
 // Routes
 
+app.get('/dashboard', function(req, res){
+	print_visits(req, res);
+	
+	/*
+  res.render('dashboard', {
+    title: 'Express',
+	locals : { temp: 1 }
+  });
+*/
+});
+
 app.get('/', function(req, res){
-  res.render('index', {
+record_visit(req, res);
+/*
+res.render('index', {
     title: 'Express'
   });
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log("Listening on " + port);
+*/
 });
 
 
-console.log("Express server listening on port %d", app.address().port);
+app.listen(port, host, function(){
+  console.log("Express server listening on port %d", app.address().port);
+});
+
+
+
+
+
+/*
+
+
+
+
+//tcp socekt server
+var tcpServer = net.createServer(function (socket) {
+  console.log('tcp server running on port 1337');
+});
+
+tcpServer.on('connection',function(socket){
+    socket.write('connected to the tcp server\r\n');
+    console.log('num of connections on port 1337: ' + tcpServer.connections);
+    
+    tcpGuests.push(socket);
+    
+    socket.on('data',function(data){
+        console.log('received on tcp socket:'+data);
+        socket.write('msg received\r\n');
+        
+        //send data to guest socket.io chat server
+        for (g in io.clients) {
+            var client = io.clients[g];
+            client.send({message:["arduino",data.toString('ascii',0,data.length)]});
+            
+        }
+    })
+});
+tcpServer.listen(1337);
+*/
--- arduino/Credentials.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CREDENTIALS_H__
-#define __CREDENTIALS_H__
-
-// Wifi parameters
-char passphrase[] = "1234123412";
-char ssid[] = "TheWorld";
-
-#endif
-
