45b1caac6e55be149bdf3871fc08f4f4e81e8356
Amit Kumar <themit@gmail.com>
2012-03-29 23:43:07 -0400

    consolidated dashboard.assistant and dashboard.index into a single view. added form-based signup and login

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,14 +3,8 @@ var app = require('../app');
  * GET home page.
  */
 exports.index = function (req, res) {
-  res.render('assistant', {
+  res.render('dashboard', {
     title: "Bitponics - Dashboard"
-  });
-};
-
-exports.assistant = function(req, res){
-  res.render('assistant', {
-    title: "Bitponics - Assistant"
   });
 };
 
