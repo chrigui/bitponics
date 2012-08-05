774a2683e96308e70a01912b5f181a99d509af2d
Amit Kumar <themit@gmail.com>
2012-08-05 16:31:57 -0400

    - some property renaming across models - moving some properties from GrowPlan to GrowSystem to allow more description & better portability of GrowSystems

--- models/growPlan.js
+++ models/growPlan.js
@@ -10,15 +10,14 @@ var GrowPlanSchema = new Schema({
 	createdByUserId: { type: ObjectId },
 	name: { type: String, required: true },
 	description: { type: String, required: true },
-	tags: [String],
+	plants: [String],
 	expertiseLevel: { type: String, enum: [
 		'beginner',
 		'intermediate',
 		'expert'
 	]},
-	growSystemType: { type: ObjectId, ref: 'GrowSystem' },
+	growSystem: { type: ObjectId, ref: 'GrowSystem' },
 	light: [{ type: ObjectId, ref: 'Light'}],
-	numberOfPlants: Number,
 	growMedium: { type: String, enum: [
 		'hydroton',
 		'cocoa chips',
@@ -28,30 +27,13 @@ var GrowPlanSchema = new Schema({
 		'rockwool',
 		'other'
 	]},
-	reservoir_size: { type: Number },
 	nutrients: [{ type: ObjectId, ref: 'Nutrient' }],
-	sensor_list: [{ type: ObjectId, ref: 'Sensor' }],
+	sensors: [{ type: ObjectId, ref: 'Sensor' }],
 	controls: [{ type: ObjectId, ref: 'Control'}],
 	phases: [{ type: ObjectId, ref: 'Phase' }]
 },
 { strict: true });
 
 GrowPlanSchema.plugin(useTimestamps);
-GrowPlanSchema.virtual('reservoirSizeWithUnits')
-	/**
-	 * Setter takes an object of the form { value: Number, unit: String}
-	 * unit must be 'liters' or 'gallons'
-	 */
-	.set(function(reservoirSizeWithUnits){
-		var unit = reservoirSizeWithUnits.unit || 'gallons',
-			value = reservoirSizeWithUnits.value;
-
-		if (unit === 'liters'){
-			// 1 liter = 0.264172052 gallons
-			this.set('reservoirSize', value * 0.264172052);
-		} else {
-			this.set('reservoirSize', value);
-		}
-	});
 
 exports.model = mongoose.model('GrowPlan', GrowPlanSchema);
