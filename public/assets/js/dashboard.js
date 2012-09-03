6a7ad5953519409a044484c19c24e4162bd68106
Amit Kumar <themit@gmail.com>
2012-09-02 23:35:20 -0400

    - explorations of data viz: concentric donut pie charts

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,120 +1,111 @@
-/**
- * Static graphs
- */
-$('#data-panels .light .data-graph').each(function(){
-    var data = [ { x: 0, y: 40 }, { x: 1, y: 45 }, { x: 1.1, y: 17 },{ x: 2, y: 17 }, {x: 2.1, y: 40 }, { x: 2.5, y: 45 }, { x: 3, y: 40 } ],
-        graph = new Rickshaw.Graph( {
-            element: $(this).get(0),
-            //width: 200, //defaults to width of container
-            height: 50,
-            series: [ {
-                    color: '#333',
-                    data: data
-            } ]
-        } );
+var sampleData = {
+    phases : [
+        {
+            name : 'Vegetative',
+            numberExpectedDays : 30,
+            dayData : [ // will be randomly populated by code
 
-    graph.render();
-});
+            ]
+        },
+        {
+            name : 'Flowering',
+            numberExpectedDays : 40,
+            dayData : [ // will be randomly populated by code
 
-$('#data-panels .ectds .data-graph').each(function(){
-    var data = [ { x: 0, y: 20 }, { x: 2, y: 25 }, { x: 3, y: 26 }],
-        graph = new Rickshaw.Graph( {
-            element: $(this).get(0),
-            //width: 200, //defaults to width of container
-            height: 50,
-            series: [ {
-                    color: '#333',
-                    data: data
-            } ]
-        } );
-
-    graph.render();
-});
-
-$('#data-panels .water .data-graph').each(function(){
-    var data = [ { x: 0, y: 20 }, { x: 2, y: 21 }, { x: 3, y: 20 }, { x: 4, y: 18 }],
-        graph = new Rickshaw.Graph( {
-            element: $(this).get(0),
-            //width: 200, //defaults to width of container
-            height: 50,
-            series: [ {
-                    color: '#333',
-                    data: data
-            } ]
-        } );
-
-    graph.render();
-});
-
-/**
- * Live graph
- * Override the Air graph w/ a live graph based on incoming (faked) data.
- * Note the FixedDuration series.
- */
-//$('#data-panels .air .data-graph').empty(); //clear out the static graph create above
-var tv = 500, //milliseconds
-    i = 0,
-    iv,
-    airSeries = new Rickshaw.Series.FixedDuration([{ name: 'one', color: '#333' }], undefined, {
-            timeInterval: tv,
-            maxDataPoints: 20,
-            timeBase: new Date().getTime() / 1000
-        }),
-    air_graph = new Rickshaw.Graph( {
-        element: document.querySelector("#data-panels .air .data-graph"),
-        height: 50,
-        series: airSeries
-    } ),
-    main_graph = new Rickshaw.Graph( {
-        element: document.querySelector("#main-data-graph"),
-        //height: 300,
-        series: new Rickshaw.Series.FixedDuration(
-            [
-                { name: 'one', color: 'rgba(0, 0, 0, .5)' }
-            ], 
-            undefined, 
-            {
-                timeInterval: tv,
-                maxDataPoints: 200,
-                timeBase: new Date().getTime() / 1000
-            }
-        )
-    } );
-
-// simulating live data updating
-iv = setInterval( function() {
-
-    var data = { one: Math.floor(Math.random() * 40) + 120 };
-
-    var randInt = Math.floor(Math.random()*100);
-    
-    //to see what its like with multiple data series, uncomment
-    //data.two = (Math.sin(i++ / 40) + 4) * (randInt + 400);
-    //data.three = randInt + 300;
-
-    air_graph.series.addData(data);
-    air_graph.render();
-
-    main_graph.series.addData(data);
-    main_graph.render();
-
-}, tv );
-
-
+            ]
+        }
+    ]
+};
 
 
 $(function () {
-    //Bitponics.socket = io.connect(Bitponics.appUrl);
-    Bitponics.socket = io.connect(window.location.origin);
+    var i = 0,
+        phase1 = sampleData.phases[0],
+        phase2 = sampleData.phases[1];
 
-    Bitponics.socket.on('sensor_event', function (data) {
-            console.log('SOCKET EVENT');
-            console.log(data);
-            var point = [];
-            point[0] = (new Date(data.timestamp)).getTime();
-            point[1] = data.value;
-            console.log(point);
-            
+    for (i = 0; i < (phase1.numberExpectedDays); i++){
+        if ( i < (phase1.numberExpectedDays / 2 ) ){
+            phase1.dayData.push(i);
+        } else {
+            phase1.dayData.push(-1);
+        }
+    }
+
+    for (i = 0; i < (phase2.numberExpectedDays); i++){
+        phase2.dayData.push(-1);
+    }
+    
+    var width,
+        height = width = 400,
+        radius = width / 2,
+        colorScale = d3.scale.category20c(),
+        equalPie = d3.layout.pie(),
+        arcSpan = 60,
+        arcMargin = 0,
+        innerArc = d3.svg.arc(),
+        outerArc = d3.svg.arc();
+    
+    // disable data sorting & force all slices to be the same size
+    equalPie.sort(null)
+        .value(function(d){
+            return 1;
         });
-    Bitponics.socket.emit('client event', { my: 'data' });
+
+
+    outerArc.outerRadius(radius)
+            .innerRadius(radius - arcSpan);
+    
+    innerArc.outerRadius(radius - arcSpan - arcMargin)
+            .innerRadius(radius - (arcSpan * 2) - arcMargin);
+
+
+    var svg = d3.select('#phases-graph')
+                    .append('svg:svg')
+                        .attr('width', width)
+                        .attr('height', height);
+
+    
+    var phase1G = svg.append('svg:g')
+                    .classed('phase1', true)
+                    .attr('transform', 'translate(' + (width / 2) + ',' + (width / 2) + ')');
+
+    var phase2G = svg.append('svg:g')
+                    .classed('phase2', true)
+                    .attr('transform', 'translate(' + (width / 2) + ',' + (width / 2) + ')');
+    
+    var getFillColor = function(data, index){
+        var num = parseInt(data.data);
+
+        if (num < 0 ) { 
+            return '#aaa';
+        } else if (num < 10){
+            return '#ccc'
+        } else {
+            return '#eee'
+        }
+
+    }
+
+    phase1G.selectAll('path')
+        .data(equalPie(phase1.dayData))
+        .enter()
+            .append('svg:path')
+            .attr('d', innerArc)
+            .attr('stroke', '#fff')
+            .attr('stroke-width', 1)
+            .attr('fill', getFillColor);
+    
+    phase2G.selectAll('path')
+        .data(equalPie(phase2.dayData))
+        .enter()
+            .append('svg:path')
+            .attr('d', outerArc)
+            .attr('stroke', '#fff')
+            .attr('stroke-width', 1)
+            .attr('fill', '#aaa');
+
+    var phase1GRotate = 0;
+    setInterval(function(){
+        phase1G.attr('transform', 'translate(' + (width / 2) + ',' + (width / 2) + ')');
+    }, 15)
 });
