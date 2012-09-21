b0dda2218cfd353a322a3b88dedc34ddf704a36c
Amit Kumar <themit@gmail.com>
2012-09-21 01:02:14 -0400

    /setup posting path for pairing device with user

--- public/assets/js/setup.js
+++ public/assets/js/setup.js
@@ -5,13 +5,39 @@ $(function(){
 		$wifiForm = $('#wifi-form'),
 		$wifiSsid = $('#wifi-ssid'),
 		$wifiPass = $('#wifi-pass'),
-		$wifiMode = $('#wifi-mode');
+		$wifiMode = $('#wifi-mode'),
+		$enterWifiData = $('#enter-wifi-data'),
+		$wifiDataSubmitted = $('#wifi-data-submitted'),
+		$submitDeviceInfo = $('#submit-device-info'),
+		$pairingComplete = $('#pairing-complete'),
+		dataToPostAfterSuccess = {
+			deviceMacAddress : ''
+		};
+		scannedWifiNetworks = [],
+		selectedWifiNetwork = {},
+		connectToDeviceRetryTimer = 60000,
+
+		securityModeOptions = {
+			'WPA_MODE' : 'WPA_MODE',
+			'WEP_MODE' : 'WEP_MODE',
+			'NONE' : 'NONE'
+		},
+		securityModeMap = {
+			'00' : securityModeOptions['WPA_MODE'],
+			'01' : securityModeOptions['WEP_MODE'],
+			'02' : securityModeOptions['WPA_MODE'],
+			'03' : securityModeOptions['WPA_MODE'],
+			'04' : securityModeOptions['WPA_MODE'],
+			'05' : securityModeOptions['NONE'],
+			'06' : securityModeOptions['WPA_MODE'],
+			'08' : securityModeOptions['WPA_MODE']
+		};
 
 
 	var postToDevice = function(){
-		var postDataStringPlainText = 'SSID=' + $('#wifi-ssid').val() + '\n' +
-			'PASS=' + $('#wifi-pass').val() + '\n' +
-			'MODE=' + $('#wifi-mode').val() + '\n' +
+		var postDataStringPlainText = 'SSID=' + selectedWifiNetwork.ssid + '\n' +
+			'PASS=' + $wifiPass.val() + '\n' +
+			'MODE=' + selectedWifiNetwork.securityMode + '\n' +
 			'SKEY=' + Bitponics.currentUser.privateDeviceKey + '\n' +
 			'PKEY=' + Bitponics.currentUser.publicDeviceKey; 
 
@@ -23,42 +49,86 @@ $(function(){
 			processData : false,
 			success: function(data){
 				console.log(data);
+				$('.selectedNetworkSsid').text(selectedWifiNetwork.ssid);
+				$wifiDataSubmitted.show();
 			},
 			error: function(jqXHR, textStatus, error){
 				console.log('error', jqXHR, textStatus, error);
-			},
-			complete : function(jqXHR, textStatus){
-				console.log('complete', jqXHR, textStatus);
+				// TODO retry a certain number of times
 			}
 		});	
 	};
 
-	$('#wifi-form').submit(function(e){
+	$submitDeviceInfo.click(function(e){
 		e.preventDefault();
+		// TODO : show spinner
+		$.ajax({
+			url: '/setup',
+			type: 'POST',
+			data: dataToPostAfterSuccess,
+			processData : false,
+			success: function(data){
+				console.log(data);
+				$pairingComplete.show();
+			},
+			error: function(jqXHR, textStatus, error){
+				console.log('error', jqXHR, textStatus, error);
+				// TODO retry a certain number of times
+			}
+		});			
+	})
+
+	$wifiForm.submit(function(e){
+		e.preventDefault();
+		selectedWifiNetwork = $.grep(scannedWifiNetworks, function(item, index){
+			return item.ssid === $wifiSsid.val();
+		});
+		// TODO : validate
 
 		postToDevice();
 	});
 
-	$('#connect-to-device').click(function(){
+	$connectToDevice.click(function(){
 		$.ajax({
 			url : deviceUrl,
 			type: 'GET',
 			timeout: 2000,
 			success: function(data){
 				console.log(data);
+				// Data will be in the following form:
+				/*
+				{ “mac”: “00:06:66:72:11:cf”,
+				  “networks”:  [ “01,06,-79,01,3104,00,00,00:26:62:96:a8:8e,5HMV5”,
+				 	            02,11,-42,03,3104,1c,00,00:1f:90:e6:9e:d2,55378008”
+						]
+				 }
+				*/
+				dataToPostAfterSuccess.deviceMacAddress = data.mac.replace(/:/g, '');
 				
-				macAddress = data.mac;
+				$.each(data.networks, function(index, networkString){
+					var parts = networkString.split(','),
+						ssid = parts[parts.length - 1],
+						securityModeKey = parts[3];
+					
+					scannedWifiNetworks.push({
+						ssid : ssid, 
+						securityMode : securityModeMap[securityModeKey]
+					});
+				});
 
-				$('#enter-wifi-data').show();
-				//postToDevice();
+				scannedWifiNetworks.sort(function(a, b){
+					return ((a.ssid < b.ssid) ? -1 : 1);
+				})
+				
+				$.each(scannedWifiNetworks, function(index, network){
+					$wifiSsid.append('<option value="' + network.ssid + '">' + network.ssid + '</option>');	
+				});
 
-
+				$enterWifiData.show();
 			},
 			error: function(jqXHR, textStatus, error){
+				// TODO retry a certain number of times
 				console.log('error', jqXHR, textStatus, error);
-			},
-			complete : function(jqXHR, textStatus){
-				console.log('complete', jqXHR, textStatus);
 			}
 		});
 	});
--- utils/db_init/seed_data/devices.js
+++ utils/db_init/seed_data/devices.js
@@ -35,5 +35,39 @@ module.exports = [
 					]
 				}
 			]
+		},
+		{
+			deviceId: "0006667288ae", //mac address
+			deviceType: "savedObjectIds['deviceTypes']['Bitponics Beta Device 1']",
+			name : "Bitponics Device 2",
+			users : [
+				"savedObjectIds['users']['amit@bitponics.com']"
+			 ],
+			owner: "savedObjectIds['users']['amit@bitponics.com']",
+			controlMap : [ 
+			  {
+			    control : "savedObjectIds['controls']['Light']",
+			    outputId : "1"
+			  },
+			  {
+			    control : "savedObjectIds['controls']['Water Pump']",
+			    outputId : "2"
+			  }
+			],
+			recentSensorLogs : [
+				{
+					timestamp: 1347364818821,
+					logs : [
+						{
+							sensor: "savedObjectIds['sensors']['vis']",
+							value: 12121
+						},
+						{
+							sensor: "savedObjectIds['sensors']['vis']",
+							value: 122222112
+						}
+					]
+				}
+			]
 		}
 	];
