6788df427ef9ba669ac3f2e985031f5f9728cd6a
Amit Kumar <themit@gmail.com>
2012-09-22 10:30:51 -0400

    - control cycle graphs - new loggly token for staging

--- config/winston-config.js
+++ config/winston-config.js
@@ -4,6 +4,7 @@ var winston = require('winston'),
       tokens : {
         local : 'c8593ee1-8a09-426f-acd6-871b70b91fd0',
         development : '5cc07897-f3f8-46ab-aaa1-888f88ae6683',
+        staging: 'fed14f46-e08e-43c5-b8c3-526c93c6f5fd',
         production : '437fee23-8d8a-4171-ab17-7e211c176003'
       }  
     };
