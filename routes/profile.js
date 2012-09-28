15fa3beea55bfad97692da042f20dc137a8a8762
Jack Bishop <jack@bitponics.com>
2012-09-28 03:29:50 -0400

    - Refactored icons to use classes instead of data attributes - Fixed some layout issues on Dashboard - Created Profile page where user can update info and save it (links from Dashboard User's name) - Starting to style login, signup, home

---
+++ routes/profile.js
@@ -0,0 +1,51 @@
+var UserModel = require('../models/user').model,
+	winston = require('winston'),
+	passport = require('passport');
+
+module.exports = function(app){
+	app.get('/profile', function (req, res){
+		if( !(req.user && req.user.id)){
+			return res.redirect('/login');
+		}
+
+		res.render('profile', {
+			title : 'Profile',
+			className : 'profile'
+		});
+	});
+
+	app.put('/profile', function (req, res, next){
+		UserModel.findById(req.user, function (err, user) {
+			if (err) { next(err); }
+
+	    user.email = req.body.email;
+	    user.name = { 
+	    	'first' : req.body.firstname,
+	    	'last' : req.body.lastname
+	    };
+	    user.locale = req.body.locale;
+	    user.phone = req.body.phone;
+	    // user.address = {
+	    // 	line1 : req.body.addressline1,
+	    // 	line2 : req.body.addressline2,
+	  		// city : req.body.city,
+	  		// state : req.body.state,
+	  		// zip : req.body.zip,
+	  		// country : req.body.country
+	    // };
+	    user.notificationPreferences = {
+		  	email: req.body.notifications_email,
+		  	sms: req.body.notifications_sms
+		  };
+
+	    user.save(function (err) {
+	      if (err) { return next(err); }
+      	res.render('profile', {
+					title : 'Profile - Updated',
+					className : 'profile',
+					user: user
+				});
+	    });
+	  });
+	});
+}
