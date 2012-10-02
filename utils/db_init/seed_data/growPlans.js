e24b56a9b7c0ace6456bbe816f0097a08386f348
Amit Kumar <themit@gmail.com>
2012-10-01 23:32:00 -0400

    moved growSystem, growMedium, and nutrient properties from GrowPlan to Phase.

--- utils/db_init/seed_data/growPlans.js
+++ utils/db_init/seed_data/growPlans.js
@@ -6,13 +6,6 @@ module.exports = [
 			description: "Growing indeterminate organic tomatoes (Redfield Beauty OG) for year round fruiting. Growing in a 3’x3’ grow bed filled with 4\" of hydroton.",
 			plants: ["tomatoes"],
 			expertiseLevel: "intermediate",
-			growSystem: "savedObjectIds['growSystems']['Drip']",
-			growMedium: "hydroton",
-			nutrients: [
-				"savedObjectIds['nutrients']['Grow']",
-				"savedObjectIds['nutrients']['Bloom']",
-				"savedObjectIds['nutrients']['Micro']"
-			],
 			sensors: [
 				"savedObjectIds['sensors']['ph']",
 				"savedObjectIds['sensors']['air']",
@@ -36,13 +29,6 @@ module.exports = [
 			description: "A grow plan suited for growing any variety of herbs and leafy greens you want. Basil, cilantro, lettuce, swiss chard, throw it all in!",
 			plants: ["basil", "swiss chard", "cilantro", "lettuce"],
 			expertiseLevel: "intermediate",
-			growSystem: "savedObjectIds['growSystems']['Drip']",
-			growMedium: "hydroton",
-			nutrients: [
-				"savedObjectIds['nutrients']['Grow']",
-				"savedObjectIds['nutrients']['Bloom']",
-				"savedObjectIds['nutrients']['Micro']"
-			],
 			sensors: [
 				"savedObjectIds['sensors']['ph']",
 				"savedObjectIds['sensors']['air']",
