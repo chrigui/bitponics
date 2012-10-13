33d09393373a54613cd2850deec8b1397c7eb820
Amit Kumar <themit@gmail.com>
2012-10-12 21:58:57 -0400

    some more placeholders for cron jobs

--- worker.js
+++ worker.js
@@ -1,4 +1,25 @@
-var cronJob = require('cron').CronJob;
-new cronJob('* * * * * *', function(){
-    console.log('You will see this message every second');
+var cronJob = require('cron').CronJob,
+	GrowPlanModel = require('models/growPlan').growPlan.model,
+	GrowPlanInstanceModel = require('models/growPlanInstance').model;
+
+/**
+ * Every hour at :00
+ * - phase advancement checks
+ *
+ */
+new cronJob('0 * * * * *', function(){
+    console.log('Every hour at :00');
+
+
+}, null, true, "America/New_York");
+
+
+/**
+ * Every 5 minutes
+ * - Clearing pending Notifications
+ */
+ new cronJob('*/5 * * * * *', function(){
+    console.log('Every 5 minutes');
+
+
 }, null, true, "America/New_York");
