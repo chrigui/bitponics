eea71168dd51c3739429880162b2586389634891
Amit Kumar <themit@gmail.com>
2012-10-14 19:03:48 -0400

    added link in admin page to trigger clearPendingNotifications

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
-var winston = require('winston');
+var winston = require('winston'),
+	ModelUtils = require('../models/utils');
 
 module.exports = function(app){
 	/*
@@ -6,8 +7,6 @@ module.exports = function(app){
 	 * Require authenticated user with property admin=true
 	 */
 	app.all('/admin*', function (req, res, next) {
-	  winston.dir(req.user);
-	  winston.dir(req.loggedIn);
 	  if (req.user.admin) {
 	    next();
 	  } else {
@@ -23,4 +22,20 @@ module.exports = function(app){
 	    title: 'Bitponics Admin'
 	  })
 	});
+
+	
+	app.get('/admin/trigger_clearPendingNotifications', function (req, res) {
+	  ModelUtils.clearPendingNotifications(require('../models/notification').model, function(err){
+	  	if (err) { 
+	  		winston.error(err); 
+	  		res.status(500);
+	        res.send('error');
+	        return;
+	  	}
+	  	res.status(200);
+	  	res.send('success');
+	  	return;
+	  });
+	});	
+
 };
