3e7d1f428302401bd95bf561623eb27b324ab1d5
Amit Kumar <themit@gmail.com>
2012-11-19 14:02:03 -0500

    expanded GrowPlan equivalence check. checking top-level phase properties & refactored to support async comparisons. still have many nested objects to check

--- models/growPlan/phase.js
+++ models/growPlan/phase.js
@@ -39,4 +39,88 @@ var PhaseSchema = new Schema({
 	nutrients : [{ type: ObjectId, ref: 'Nutrient', required: false }],
 });
 
+
+/************************** INSTANCE METHODS  ***************************/
+/*
+ * Given another Phase object, determine whether
+ * they're equivalent..
+ * Comparing only salient properties; ignoring properties 
+ * like createdAt/updatedAt
+ * 
+ * @param other. Phase model object
+ * @param callback. Function to be called with result. Passed a boolean argument,
+ * 					true if the objects are equivalent, false if not
+ *
+ */
+PhaseSchema.method('isEquivalentTo', function(other, callback){
+	var phase = this;
+
+	// compare name
+	if (this.name !== other.name) { return callback(null, false); }
+
+	// compare description
+	if (this.description !== other.description) { return callback(null, false); }
+
+	// compare expectedNumberOfDays
+	if (this.expectedNumberOfDays !== other.expectedNumberOfDays) { return callback(null, false); }
+
+	// compare growMedium
+	if (this.growMedium !== other.growMedium) { return callback(null, false); }	
+
+	// compare phaseEndDescription
+	if (this.phaseEndDescription !== other.phaseEndDescription) { return callback(null, false); }		
+
+
+	// compare light, shallow
+	if ( !(
+			(this.light.fixture && other.light.fixture) ||
+			(!this.light.fixture && !other.light.fixture)
+		 )
+		){ 
+		return callback(null, false); 
+	}
+
+	// compare actions, shallow
+	if ( !(
+			(this.actions && other.actions) ||
+			(!this.actions && !other.actions)
+		 )
+		){ 
+		return callback(null, false); 
+	}
+	if (this.actions && other.actions && (this.actions.length !== other.actions.length)){
+		return callback(null, false);
+	}
+
+	// compare idealRanges, shallow
+	if ( !(
+			(this.idealRanges && other.idealRanges) ||
+			(!this.idealRanges && !other.idealRanges)
+		 )
+		){ 
+		return callback(null, false); 
+	}
+	if (this.idealRanges && other.idealRanges && (this.idealRanges.length !== other.idealRanges.length)){
+		return callback(null, false);
+	}
+	
+
+	// compare nutrients, shallow
+	if ( !(
+			(this.nutrients && other.nutrients) ||
+			(!this.nutrients && !other.nutrients)
+		 )
+		){ 
+		return callback(null, false); 
+	}
+	if (this.nutrients && other.nutrients && (this.nutrients.length !== other.nutrients.length)){
+		return callback(null, false);
+	}
+	
+
+	// TODO : all the async comparisons
+
+	return callback(null, true);
+});
+
 exports.schema = PhaseSchema;
