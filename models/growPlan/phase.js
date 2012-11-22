1cd092a5b8d42ba51b8a936622f3c61d65cf8132
Amit Kumar <themit@gmail.com>
2012-11-21 20:48:37 -0500

    completed equivalence check of phases. just comparing ID's on all nested objects. should be cool. still need to figure out how to check whether we're dealing with a populated Model or an ObjectId

--- models/growPlan/phase.js
+++ models/growPlan/phase.js
@@ -72,13 +72,29 @@ PhaseSchema.method('isEquivalentTo', function(other, callback){
 
 
 	// compare light, shallow
-	if ( !(
+	if ( this.light && !other.light){ return callback(null, false); }
+	if ( !this.light && other.light){ return callback(null, false); }
+	if ( this.light && other.light){
+		if ( !(
 			(this.light.fixture && other.light.fixture) ||
 			(!this.light.fixture && !other.light.fixture)
 		 )
 		){ 
-		return callback(null, false); 
+			return callback(null, false); 
+		}
+		if ( !(
+			(this.light.bulb && other.light.bulb) ||
+			(!this.light.bulb && !other.light.bulb)
+		 )
+		){ 
+			return callback(null, false); 
+		}
+		if ( this.light.fixtureQuantity !== other.light.fixtureQuantity)
+		{ 
+			return callback(null, false); 
+		}
 	}
+	
 
 	// compare actions, shallow
 	if ( !(
@@ -91,6 +107,19 @@ PhaseSchema.method('isEquivalentTo', function(other, callback){
 	if (this.actions && other.actions && (this.actions.length !== other.actions.length)){
 		return callback(null, false);
 	}
+
+	// compare phaseEndActions, shallow
+	if ( !(
+			(this.phaseEndActions && other.phaseEndActions) ||
+			(!this.phaseEndActions && !other.phaseEndActions)
+		 )
+		){ 
+		return callback(null, false); 
+	}
+	if (this.phaseEndActions && other.phaseEndActions && (this.phaseEndActions.length !== other.phaseEndActions.length)){
+		return callback(null, false);
+	}
+
 
 	// compare idealRanges, shallow
 	if ( !(
@@ -119,8 +148,134 @@ PhaseSchema.method('isEquivalentTo', function(other, callback){
 	
 
 	// TODO : all the async comparisons
+	var allAsyncEquivalenceChecksPassed = true;
+	async.parallel(
+		[
+			function lightComparison(innerCallback){
+				if (!phase.light){ return innerCallback(null, true); }
 
-	return callback(null, true);
+				if (phase.light.fixture){
+					if (!phase.light.fixture._id.equals(other.light.fixture._id)){
+						return innerCallback(null, false);
+					}
+				}
+				if (phase.light.fixtureQuantity !== other.light.fixtureQuantity){
+					return innerCallback(null, false);
+				}
+				if (phase.light.bulb){
+					if (!phase.light.bulb._id.equals(other.light.bulb._id)){
+						return innerCallback(null, false);
+					}
+				}
+				return innerCallback(null, true);
+			},
+			function actionsComparison(innerCallback){
+				if (!phase.actions.length) { return callback(null, true); }
+
+				var allActionsFound = true;
+				for (var i = 0, length = phase.actions.length; i < length; i++){
+					// TODO : figure out how to handle actions that haven't been "populated" (they're still just an ObjectID)
+					var actionId = phase.actions[i]._id,
+						actionFound = false;
+					for (var j = 0; j < length; j++){
+						if (actionId.equals(other.actions[j]._id)){
+							actionFound = true;
+							break;
+						}
+					}
+					if (!actionFound) { 
+						allActionsFound = false;
+						break;
+					}
+				}
+				if (!allActionsFound){
+					return callback(null, false);
+				}
+				return callback(null, true);
+			},
+			function phaseEndActionsComparison(innerCallback){
+				if (!phase.phaseEndActions.length) { return callback(null, true); }
+
+				var allActionsFound = true;
+				for (var i = 0, length = phase.phaseEndActions.length; i < length; i++){
+					// TODO : figure out how to handle actions that haven't been "populated" (they're still just an ObjectID)
+					var actionId = phase.phaseEndActions[i]._id,
+						actionFound = false;
+					for (var j = 0; j < length; j++){
+						if (actionId.equals(other.phaseEndActions[j]._id)){
+							actionFound = true;
+							break;
+						}
+					}
+					if (!actionFound) { 
+						allActionsFound = false;
+						break;
+					}
+				}
+				if (!allActionsFound){
+					return callback(null, false);
+				}
+				return callback(null, true);	
+			},
+			function idealRangesComparison(innerCallback){
+				if (!phase.idealRanges.length) { return callback(null, true); }
+
+				var allIdealRangesFound = true;
+				for (var i = 0, length = phase.idealRanges.length; i < length; i++){
+					// TODO : figure out how to handle those that haven't been "populated" (they're still just an ObjectID)
+					var idealRangeId = phase.idealRanges[i]._id,
+						idealRangeFound = false;
+					for (var j = 0; j < length; j++){
+						if (idealRangeId.equals(other.idealRanges[j]._id)){
+							idealRangeFound = true;
+							break;
+						}
+					}
+					if (!idealRangeFound) { 
+						allIdealRangesFound = false;
+						break;
+					}
+				}
+				if (!allIdealRangesFound){
+					return callback(null, false);
+				}
+				return callback(null, true);
+			},
+			function nutrientsComparison(innerCallback){
+				if (!phase.nutrients.length) { return callback(null, true); }
+
+				var allNutrientsFound = true;
+				for (var i = 0, length = phase.nutrients.length; i < length; i++){
+					// TODO : figure out how to handle those that haven't been "populated" (they're still just an ObjectID)
+					var nutrientId = phase.nutrients[i]._id,
+						nutrientFound = false;
+					for (var j = 0; j < length; j++){
+						if (nutrientId.equals(other.nutrients[j]._id)){
+							nutrientFound = true;
+							break;
+						}
+					}
+					if (!nutrientFound) { 
+						allNutrientsFound = false;
+						break;
+					}
+				}
+				if (!allNutrientsFound){
+					return callback(null, false);
+				}
+				return callback(null, true);	
+			}
+		],
+		function parallelComparisonEnd(err, results){
+			for (var i = 0, length = results.length; i < length; i++){
+				if (results[i] === false){ 
+					allAsyncEquivalenceChecksPassed = false; 
+					break;
+				}
+			}
+			return callback(err, allAsyncEquivalenceChecksPassed);
+		}
+	);
 });
 
 exports.schema = PhaseSchema;
