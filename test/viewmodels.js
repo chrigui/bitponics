a7575854473e41ac8d72cb565c9668cd652f4b7b
Amit Kumar <themit@gmail.com>
2013-01-23 21:28:53 -0500

    Action viewModel conversions, accompanying unit tests and refactoring isEquivalentTo checks
    - changed all of the "isEquivalentTo" checks to be static methods instead of instance methods so that they could accept POJO models. Did this as a workaround for Mongoose not supporting assigning models to ObjectId reference fields.

--- test/viewmodels.js
+++ test/viewmodels.js
@@ -1,6 +1,10 @@
 var should = require('should'),
 requirejs = require('requirejs'),
-sampleGrowPlans = require('../utils/db_init/seed_data/growPlans');
+Models = require('../models'),
+sampleGrowPlans = require('../utils/db_init/seed_data/growPlans'),
+sampleActions = require('../utils/db_init/seed_data/actions'),
+Action = Models.action,
+ModelUtils = Models.utils;
 
 requirejs.config({
 	nodeRequire: require,
@@ -9,5 +13,13 @@ requirejs.config({
 var viewModels = requirejs('viewmodels');
 
 describe('ViewModels', function(){
-
+	it('converts a single-state Action server model to a viewmodel and back to an equivalent Action server model', function(done){
+		Action.findById('506de2fb8eebf7524342cb28', function(err, actionResult){
+			should.exist(actionResult);
+			var actionViewModel = viewModels.initActionViewModel(actionResult),
+				viewModelConvertedToServerModel = viewModels.compileActionViewModelToServerModel(actionViewModel);
+			Action.isEquivalentTo(actionResult, viewModelConvertedToServerModel).should.be.true;
+			done();
+		});
+	});
 });
