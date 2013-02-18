aa26a98fbe71a8a00c81feb3b8b1a298436a2a23
Jack Bishop <jack@rga.com>
2013-02-18 14:37:00 -0500

    Clean up; Mobile nav ready for toggling; legacy routes set up;

---
+++ routes/legacy.js
@@ -0,0 +1,35 @@
+module.exports = function (app){
+
+  app.get('/index', function (req, res){
+    res.redirect('/');
+  });
+
+  app.get('/index.php', function (req, res){
+    res.redirect('/index');
+  });
+
+  app.get('/how', function (req, res){
+    res.redirect('/howitworks');
+  });
+
+  app.get('/how.php', function (req, res){
+    res.redirect('/howitoworks');
+  });
+
+  app.get('/photos', function (req, res){
+    res.redirect('/');
+  });
+
+  app.get('/photos.php', function (req, res){
+    res.redirect('/');
+  });
+
+  app.get('/press.php', function (req, res){
+    res.redirect('/press');
+  });
+
+  app.get('/contact.php', function (req, res){
+    res.redirect('/contact');
+  });
+
+}
