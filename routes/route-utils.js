49334e6e99fb5139ae26621123c1171d952bfe3c
Amit Kumar <themit@gmail.com>
2013-02-20 00:06:34 -0500

    new middleware utility to ensure user is logged in

--- routes/route-utils.js
+++ routes/route-utils.js
@@ -1,6 +1,10 @@
-exports = {
-	ensureAuthenticated : function(res, req, next){
-		if (req.isAuthenticated()) { return next(); }
-  		res.redirect('/login')
+module.exports = {
+	middleware : {
+		ensureLoggedIn : function(req, res, next){
+			if( !(req.user && req.user.id)){
+				return res.redirect('/login?redirect=' + req.originalUrl);
+			}
+			next();
+		}
 	}
 };
