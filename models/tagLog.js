2abbdd717571dd872173d4f6eeba8dadf6c91233
Amit Kumar <themit@gmail.com>
2013-03-06 00:46:49 -0500

    db-only-optimized props for TagLog + virtuals & unit tests

--- models/tagLog.js
+++ models/tagLog.js
@@ -3,6 +3,37 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
 	ObjectId = Schema.ObjectId;
 
+
+
+var TagReadingSchema = new Schema({
+    /**
+     * Value.
+     */
+      v: { type : String, required: true},
+      t: { type : [String]}
+},
+  /**
+   * Prevent the _id property since these will only ever be subdocs in HarvestLog, don't need
+   * ObjectIds created on them
+   */
+  { _id : false, id : false } );
+
+TagReadingSchema.virtual('val')
+  .get(function(){
+    return this.v;
+  })
+  .set(function(val){
+    this.v = val;
+  });
+TagReadingSchema.virtual('tags')
+  .get(function(){
+    return this.t;
+  })
+  .set(function(tags){
+    this.t = tags;
+  });
+
+
 /**
  * TagLog
  */
@@ -17,14 +48,48 @@ var TagLogSchema = new Schema({
 	 */
 	ts: { type: Date, required: true, default: Date.now },
 
-	logs : [{
-		val: { type : String, required: true},
-		tags: { type : [String]}
-	}]
+	l : [TagReadingSchema]
 },
 { id : false });
 
+
+TagLogSchema.virtual('logs')
+  .get(function(){
+    return this.l;
+  })
+  .set(function(logs){
+    this.l = logs;
+  });
+
+
+/*************** SERIALIZATION *************************/
+
+/**
+ * Remove the db-only-optimized property names and expose only the friendly names
+ *
+ * "Transforms are applied to the document and each of its sub-documents"
+ * http://mongoosejs.com/docs/api.html#document_Document-toObject
+ */
+TagLogSchema.set('toObject', {
+  getters : true,
+  transform : function(doc, ret, options){
+    if (doc.schema === TagReadingSchema){
+      delete ret.v;
+      delete ret.t;
+    } else {
+      // else we're operating on the parent doc (the SensorLog doc)
+      delete ret.l;
+    }
+  }
+});
+TagLogSchema.set('toJSON', {
+  getters : true,
+  transform : TagLogSchema.options.toObject.transform
+});
+/*************** END SERIALIZATION *************************/
+
+
 TagLogSchema.index({ 'gpi ts logs.tags': -1 });
 
-exports.schema = TagLogchema;
+exports.schema = TagLogSchema;
 exports.model = mongoose.model('TagLog', TagLogSchema);
