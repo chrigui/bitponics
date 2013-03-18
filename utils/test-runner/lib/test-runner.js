1b7a46eb063bde0c8694bf5a98b9c382fcd62a91
Amit Kumar <themit@gmail.com>
2013-03-17 21:42:40 -0400

    created a file-watching unit test runner

---
+++ utils/test-runner/lib/test-runner.js
@@ -0,0 +1,74 @@
+#! /usr/bin/env node
+
+
+
+var childProcess = require('child_process');
+
+var running = false,
+    queued = false;
+
+var fs = require("fs");
+
+
+var runTests = function(){
+  if (!running){
+    running = true;
+
+    console.log("Starting new test run at " + (new Date()).toLocaleTimeString());
+    childProcess.exec('npm test', { stdio : "inherit"}, function(err, stdout, stderr){
+      console.log(stdout);
+      console.log("Ended test run at " + (new Date()).toLocaleTimeString());
+      running = false;
+      if (queued) {
+        queued = false;
+        runTests();
+      }
+    });
+  } else {
+    queued = true;
+  }
+};
+
+
+fs.watch("./models",function(event,file) {
+  runTests();
+});
+
+fs.watch("./test",function(event,file) {
+  runTests();
+});
+
+fs.watch("./i18n",function(event,file) {
+  runTests();
+});
+
+fs.watch("./lib",function(event,file) {
+  runTests();
+});
+
+fs.watch("./config",function(event,file) {
+  runTests();
+});
+
+runTests();
+
+
+/*
+var spawn = require('child_process').spawn;
+var testRunner = spawn('npm test');
+
+
+testRunner.stdout.on('data', function (data) {
+  console.log(data);
+});
+
+testRunner.stderr.on('data', function (data) {
+  console.log('test-runner stderr: ' + data);
+});
+
+testRunner.on('close', function (code) {
+  if (code !== 0) {
+    console.log('test-runner process exited with code ' + code);
+  }
+});
+*/
