70ad9fa042cf1b0e2c1fac58c4729efc54731643
Amit Kumar <themit@gmail.com>
2013-04-13 01:51:22 -0400

    troubleshooting a circleci testing error

--- test/models/utils.js
+++ test/models/utils.js
@@ -1,4 +1,18 @@
-var mongoose = require('mongoose'),
+
+
+/*
+ * Mocha Test
+ *
+ * Tests are organized by having a "describe" and "it" method. Describe
+ * basically creates a "section" that you are testing and the "it" method
+ * is what runs your test code.
+ *
+ * For asynchronous tests you need to have a done method that you call after
+ * your code should be done executing so Mocha runs to test properly.
+ */
+
+describe('Model Utils', function(){
+	var mongoose = require('mongoose'),
   ObjectID = require('mongodb').ObjectID,
   ImmediateAction = require('../../models/immediateAction').model,
   GrowPlanInstance = require('../../models/growPlanInstance').model,
@@ -16,18 +30,6 @@ var mongoose = require('mongoose'),
   feBeUtils = requirejs('fe-be-utils');
 
 
-/*
- * Mocha Test
- *
- * Tests are organized by having a "describe" and "it" method. Describe
- * basically creates a "section" that you are testing and the "it" method
- * is what runs your test code.
- *
- * For asynchronous tests you need to have a done method that you call after
- * your code should be done executing so Mocha runs to test properly.
- */
-
-describe('Model Utils', function(){
 
   describe('.triggerImmediateAction', function(){
     
