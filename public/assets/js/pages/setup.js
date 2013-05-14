d4deabbae8a8049c3ae2ab4bd8e91aecd27d0158
Amit Kumar <themit@gmail.com>
2013-05-14 01:31:52 -0400

    serial input mask on setup page

--- public/assets/js/pages/setup.js
+++ public/assets/js/pages/setup.js
@@ -1,30 +1,49 @@
 require([
   'angular',
   'domReady',
-  // 'moment',
-  // 'fe-be-utils',
-  // 'view-models',
   'angularResource',
-  // 'd3',
   'es5shim',
-  // 'steps',
-  // 'overlay'
+  'angularUI',
+  '/assets/js/services/socket.js',
+  '/assets/js/libs/angular/mask.js'
 ],
-// function (angular, domReady, moment, feBeUtils, viewModels) {
 function (angular, domReady) {
   'use strict';
 
-  var setupApp = angular.module('bpn.apps.setup', ['ngResource']);
+  var setupApp = angular.module('bpn.apps.setup', ['ui.mask', 'ngResource', 'bpn.services']);
+
+
+  setupApp.directive('uppercase', function() {
+     return {
+       priority: 200, // give it higher priority than the input mask
+       require: 'ngModel',
+       link: function(scope, element, attrs, modelCtrl) {
+          var uppercase = function(inputValue) {
+             var upperCased = inputValue.toUpperCase()
+             if(upperCased !== inputValue) {
+                modelCtrl.$setViewValue(upperCased);
+                modelCtrl.$render();
+              }         
+              return upperCased;
+           }
+           modelCtrl.$parsers.push(uppercase);
+           uppercase(scope[attrs.ngModel]); 
+       }
+     };
+  });
 
   setupApp.controller('bpn.controllers.setup.Main',
     [
       '$scope',
       '$filter',
       '$http',
-      function ($scope, $filter, $http) {
+      'bpn.services.socket',
+      function ($scope, $filter, $http, socket) {
         $scope.serialServiceURL = undefined;
-        $scope.serial = undefined;
+        $scope.serial = "";
         $scope.key = undefined;
+        $scope.socket = socket;
+        $scope.serialMask = "**-***-****";
 
         $scope.sendSerialToServer = function() {
           $http.post("/setup", { 'serial': $scope.serial })
@@ -39,7 +58,17 @@ function (angular, domReady) {
 
         $scope.openWifiPairingPage = function() {
           window.open('http://' + bpn.pageData.nextUrl, "device");
+
+          $scope.socket.connect('/setup');
+          $scope.socket.on('device_calibration_response', function (data) {
+          });
+          $scope.socket.emit('ready');
         }
+
+        $scope.$watch('serial', function(oldValue, newValue){
+          //console.log($filter('uppercase')($scope.serial));
+          //$scope.serial = $scope.serial.toUpperCase();
+        });
       }
     ]
   );
