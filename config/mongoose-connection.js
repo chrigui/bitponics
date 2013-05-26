ba8141923f7b618e56a5017630e8623d4a4eebf8
Amit Kumar <themit@gmail.com>
2013-05-26 17:42:46 -0400

    production connection string cleanup

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -3,7 +3,8 @@
  * Subsequent requires can simply call require('mongo-connection').defaultConnection
  */
 
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+		winston = require('winston');
 
 var self = {
 	
@@ -15,8 +16,7 @@ var self = {
 		local : 'mongodb://localhost/bitponics-local',
 		development : 'mongodb://admin:1SHar3db1t@ds037597.mongolab.com:37597/bitponics-development',
 		staging : 'mongodb://admin:1SHar3db1t@ds037617.mongolab.com:37617/bitponics-staging',
-  	//production : 'mongodb://admin%40bitponics.com:1SHar3db1t@ds041507-a0.mongolab.com:41507,ds041507-spare.mongolab.com:41507/bitponics-prod',
-  	production : 'mongodb://admin:1SHar3db1t@ds037617.mongolab.com:37617/bitponics-staging',
+  	production : 'mongodb://admin%40bitponics.com:1SHar3db1t@ds041507-a0.mongolab.com:41507,ds041507-spare.mongolab.com:41507/bitponics-prod',
   	test : 'mongodb://localhost/bitponics-test',
   	localLocal : 'mongodb://localhost/bitponics-local'
 	},
@@ -42,17 +42,23 @@ var self = {
 
 	open : function(environment, callback){
 		
-		var connection = self.connections[environment];
+		var connection = self.connections[environment],
+				url;
 		
 		if (connection){
+			winston.info("DATABASE RETURNING OPEN CONNECTION TO " + environment);
 			if (callback){
 				callback(connection);
 			}
 			return connection;
 		}
 
+		url = self.urls[environment] || environment;
+
+		winston.info("DATABASE OPENING CONNECTION TO " + url);
+
 		connection = mongoose.createConnection(
-			self.urls[environment], 
+			url, 
 			self.connectionOptions,
 			function(){
 				if (callback){
