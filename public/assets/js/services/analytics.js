1d33dd834f5a55ce47a061187a8f5a89fd88a756
Amit Kumar <themit@gmail.com>
2013-12-05 05:36:05 -0500

    mixpanel refactoring, triggering pageview events

--- public/assets/js/services/analytics.js
+++ public/assets/js/services/analytics.js
@@ -1,17 +1,27 @@
+
 define([
-	'bpn.services',
-	'mixpanel'
+	'bpn.services'
 	], 
 	function (bpnServices, mixpanel) {
 		'use strict';
 
 		// http://stackoverflow.com/questions/10713708/tracking-google-analytics-page-views-with-angular-js
 		return bpnServices.service('bpn.services.analytics', [
-	    '$rootScope', '$window', '$location', function($rootScope, $window, $location) {
+	    '$rootScope', 
+      '$window', 
+      '$location',
+      '$document',
+      function($rootScope, $window, $location, $document) {
 	      var track = function() {
-	        $window._gaq.push(['_trackPageview', $location.path()]);
+	        console.log("LOGGING PAGEVIEW: " + $location.path());
+          
+          $window._gaq.push(['_trackPageview', $location.path()]);
+
+          $window.mixpanel.track("page viewed", {'page name': $window.document.title, 'url' : $location.path() });
 	      };
 	      $rootScope.$on('$viewContentLoaded', track);
+
+                
 	    }
 	  ]);
 	}
