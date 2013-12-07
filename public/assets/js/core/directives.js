29dc8d7a00fca2006eb06d5de6936447d37fed7f
Amit Kumar <themit@gmail.com>
2013-12-07 01:24:36 -0500

    polyfill contenteditable to be a directive automatically bound to ng-model

--- public/assets/js/core/directives.js
+++ public/assets/js/core/directives.js
@@ -105,6 +105,45 @@ define(['angular'],
     ]);
 
 
+    /** 
+     * This overrides the default contenteditable to be one that can be bound to ng-model
+     * 
+     * taken from http://docs.angularjs.org/api/ng.directive:ngModel.NgModelController
+     * with the following changes:
+     * - prevent assigning of <br> by default
+     * - prevent automatic read() on creation
+     */
+    bpnDirectives.directive('contenteditable', function() {
+      return {
+        restrict: 'A', // only activate on element attribute
+        require: '?ngModel', // get a hold of NgModelController
+        link: function(scope, element, attrs, ngModel) {
+  
+          if(!ngModel) return; // do nothing if no ng-model
+  
+          // Specify how UI should be updated
+          ngModel.$render = function() {
+            element.html(ngModel.$viewValue || '');
+          };
+   
+          // Listen for change events to enable binding
+          element.on('blur keyup', function() {
+            scope.$apply(read);
+          });
+          
+          // Write data to the model
+          function read() {
+            var html = element.html();
+            // When we clear the content editable the browser leaves a <br> behind
+            if( html == '<br>' ) {
+              html = '';
+            }
+            ngModel.$setViewValue(html);
+          }
+        }
+      };
+    });
+
     return bpnDirectives;
 	}
 );
