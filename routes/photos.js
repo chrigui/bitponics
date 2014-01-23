9123de6802b265c52c73eee310850fb8cf29bfc2
Amit Kumar <themit@gmail.com>
2014-01-23 01:54:42 -0500

    WIP: showing plant photo in grow plan card

--- routes/photos.js
+++ routes/photos.js
@@ -19,65 +19,7 @@ module.exports = function(app){
 	 * Verifies that the requesting user has access to the photo.
 	 * Returns a redirect to the image asset on S3
 	 */
-	app.get('/photos/:photoId/:size?', function (req, res, next){
-			
-		async.waterfall(
-			[
-				function checkAuth(innerCallback){
-					PhotoModel.findById(req.params.photoId)
-					.select('owner ref.documentId visibility')
-					.lean()
-					.exec(function(err, photoResult){
-						if (err) { return innerCallback(err); }
-						
-            // to just return redirect URL regardless:
-						// if (!photoResult){ return innerCallback(null, true);}
-						
-            if (!photoResult){ return innerCallback(new Error("Invalid photo id"));}
-						
-						// first check access on the photo
-            if (routeUtils.checkResourceReadAccess(photoResult, req.user)){
-							return innerCallback(null, true);
-						}
-						
-            // if that returns false, maybe the user has access by being a member of the referenced resource
-						if (photoResult.ref.collectionName){
-	            ModelUtils.getModelFromCollectionName(photoResult.ref.collectionName).findById(photoResult.ref.documentId)
-              .select('owner users visibility')
-							.lean()
-							.exec(function(err, referenceDocumentResult){
-								if (err) { return innerCallback(err); }
-								return innerCallback(null, routeUtils.checkResourceReadAccess(referenceDocumentResult, req.user));
-							});
-						} 
-						else {
-							return innerCallback(null, false);
-						}
-					});
-				}
-			],
-			function (err, isAuthorized){
-				if (err) { return next(err); }
-				if (isAuthorized){
-					// Create a fast-expiring signed S3 url
-					// http://www.arbitrarytech.com/2013/03/nodejs-library-for-client-side-uploads.html
-					var s3PhotoPath = s3Config.photoPathPrefix + req.params.photoId;
-          if (req.params.size){
-            s3PhotoPath += '/' + req.params.size;
-          }
-
-          var	url = policyGenerator.readPolicy(s3PhotoPath, s3Config.bucket, expirationSeconds);
-
-					// let the browser cache for expirationSeconds
-					res.setHeader("Expires", (new Date(Date.now() + (expirationSeconds * 1000))).toUTCString());
-					return res.redirect(302, url);	
-				} else {
-					// TODO : create a stylized 401 page
-					return res.send(401);	
-				}
-			}
-		);
-	});
+	app.get('/photos/:photoId/:size?', routeUtils.getPhoto);
 
   
   /**
--- routes/route-utils.js
+++ routes/route-utils.js
@@ -205,6 +205,83 @@ module.exports = {
   },
 
 
+  getPhoto : function(req, res, next){
+    var async = require('async'),
+      winston = require('winston'),
+      routeUtils = require('./route-utils'),
+      mongoose = require('mongoose'),
+      ModelUtils = require('../models/utils'),
+      PhotoModel = require('../models/photo').model,
+      GrowPlanInstanceModel = require('../models/growPlanInstance').model,
+      routeUtils = require('./route-utils'),
+      s3Config = require('../config/s3-config'),
+      s3Policy = require('s3policy'),
+      policyGenerator = new s3Policy(s3Config.key, s3Config.secret),
+      expirationSeconds = 10;
+      requirejs = require('../lib/requirejs-wrapper'),
+      feBeUtils = requirejs('fe-be-utils'),
+      filesKeys = Object.keys(req.files),
+      now = new Date();
+
+    async.waterfall(
+      [
+        function checkAuth(innerCallback){
+          PhotoModel.findById(req.params.photoId)
+          .select('owner ref.documentId visibility')
+          .lean()
+          .exec(function(err, photoResult){
+            if (err) { return innerCallback(err); }
+            
+            // to just return redirect URL regardless:
+            // if (!photoResult){ return innerCallback(null, true);}
+            
+            if (!photoResult){ return innerCallback(new Error("Invalid photo id"));}
+            
+            // first check access on the photo
+            if (routeUtils.checkResourceReadAccess(photoResult, req.user)){
+              return innerCallback(null, true);
+            }
+            
+            // if that returns false, maybe the user has access by being a member of the referenced resource
+            if (photoResult.ref.collectionName){
+              ModelUtils.getModelFromCollectionName(photoResult.ref.collectionName).findById(photoResult.ref.documentId)
+              .select('owner users visibility')
+              .lean()
+              .exec(function(err, referenceDocumentResult){
+                if (err) { return innerCallback(err); }
+                return innerCallback(null, routeUtils.checkResourceReadAccess(referenceDocumentResult, req.user));
+              });
+            } 
+            else {
+              return innerCallback(null, false);
+            }
+          });
+        }
+      ],
+      function (err, isAuthorized){
+        if (err) { return next(err); }
+        if (isAuthorized){
+          // Create a fast-expiring signed S3 url
+          // http://www.arbitrarytech.com/2013/03/nodejs-library-for-client-side-uploads.html
+          var s3PhotoPath = s3Config.photoPathPrefix + req.params.photoId;
+          if (req.params.size){
+            s3PhotoPath += '/' + req.params.size;
+          }
+
+          var url = policyGenerator.readPolicy(s3PhotoPath, s3Config.bucket, expirationSeconds);
+
+          // let the browser cache for expirationSeconds
+          res.setHeader("Expires", (new Date(Date.now() + (expirationSeconds * 1000))).toUTCString());
+          return res.redirect(302, url);  
+        } else {
+          // TODO : create a stylized 401 page
+          return res.send(401); 
+        }
+      }
+    );
+  },
+
+
   /**
    * Utility method for processing photo uploads.
    * We have multiple routes that a user can upload a photo at 
