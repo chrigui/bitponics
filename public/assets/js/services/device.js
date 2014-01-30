13ab69fd021609f6d67101168bceefa8562731a3
Jack Bishop <jack.bishop1@gmail.com>
2014-01-30 16:24:11 -0500

    Device page and overlay style updates

--- public/assets/js/services/device.js
+++ public/assets/js/services/device.js
@@ -11,19 +11,22 @@ define([
 					return $resource('/api/devices/:id', 
 						{ id: '@_id'},
 						{
-							update: { method:'PUT', isArray: false},
-              save: { 
-                method:'POST', 
-                isArray: false,
-                url: '/api/devices/'
-              },
-              saveSensors : {
-                method:'PUT', 
-                transformRequest: function (data, headersGetter) {
-                  var json = JSON.stringify({ sensors: data.sensors });
-                  return json;
-                }
-              }
+						  update: { 
+						  	method:'PUT', 
+						  	isArray: false
+						  },
+			              save: { 
+			                method:'POST', 
+			                isArray: false,
+			                url: '/api/devices/'
+			              },
+			              saveSensors : {
+			                method:'PUT', 
+			                transformRequest: function (data, headersGetter) {
+			                  var json = JSON.stringify({ sensors: data.sensors });
+			                  return json;
+			                }
+			              }
 						}
 					);
 				}
