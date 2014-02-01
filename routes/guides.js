423a159ae8c9b434ebd26d873b0a396a9760b6bc
Amit Kumar <themit@gmail.com>
2014-02-01 17:50:30 -0500

    placeholder index page for /guides

--- routes/guides.js
+++ routes/guides.js
@@ -1,11 +1,18 @@
 module.exports = function(app){
+
 	app.get('/guides/water-culture-system', function (req, res){
-	  //print_visits(req, res);
 	  res.render('guides/waterculturesystem', {
 	    title: "Bitponics Water Culture System",
     	className: "landing-page single-page guides water-culture-system",
 		pageType: "landing-page"
 	  });
-
 	});
+
+  app.get('/guides', function (req, res){
+    res.render('guides', {
+      title: "Bitponics Guides",
+      className: "landing-page single-page",
+      pageType: "landing-page"
+    });
+  });
 };
