30a8ca017db5a9e07cd4d7dcea685a2cd74943e8
Amit Kumar <themit@gmail.com>
2014-03-30 16:12:04 -0400

    disabling FTP photo fetching in worker. not in use anymore

--- worker-process.js
+++ worker-process.js
@@ -1,17 +1,17 @@
 #! /usr/bin/env node
 
 var environment = process.argv.slice(2)[0], //gets first cmd line arg
-	cronJob = require('cron').CronJob,
-	winston = require('winston'),
-	mongoose = require('mongoose'),
-	async = require('async'),
-	moment = require('moment'),
-	winston = require('winston'),
-	mongooseConnection = require('./config/mongoose-connection').open(environment),
-	ModelUtils = require('./models/utils'),
-	NotificationModel = require('./models/notification').model,
-	GrowPlanInstanceModel = require('./models/garden').model,
-	PhotoModel = require('./models/photo').model,
+  cronJob = require('cron').CronJob,
+  winston = require('winston'),
+  mongoose = require('mongoose'),
+  async = require('async'),
+  moment = require('moment'),
+  winston = require('winston'),
+  mongooseConnection = require('./config/mongoose-connection').open(environment),
+  ModelUtils = require('./models/utils'),
+  NotificationModel = require('./models/notification').model,
+  GrowPlanInstanceModel = require('./models/garden').model,
+  PhotoModel = require('./models/photo').model,
     winston = require('./config/winston-config')('worker');
 
 
@@ -32,10 +32,10 @@ new cronJob('0 0 0 * * *', function(){
     winston.info(environment + ' ModelUtils.scanForPhaseChanges');
     
     ModelUtils.scanForPhaseChanges(GrowPlanInstanceModel, function(err, count){
-    	if (err) { winston.info(err); }
-    	var finishedEnvironmentAt = moment();
-    	winston.info(environment + ' ModelUtils.scanForPhaseChanges started at ' + now.format() + ', ended at ' + finishedEnvironmentAt.format() + ', duration ' + now.diff(finishedEnvironmentAt) + 'ms');
-    	winston.info((count || 0) + " records affected");
+      if (err) { winston.info(err); }
+      var finishedEnvironmentAt = moment();
+      winston.info(environment + ' ModelUtils.scanForPhaseChanges started at ' + now.format() + ', ended at ' + finishedEnvironmentAt.format() + ', duration ' + now.diff(finishedEnvironmentAt) + 'ms');
+      winston.info((count || 0) + " records affected");
     });
     
 }, null, true, "America/New_York");
@@ -64,26 +64,26 @@ new cronJob('0 0 0 * * *', function(){
 
 /**
  * Every 10 minutes
- * - Fetch FTP photos
+ * // - Fetch FTP photos
  * - Fetch email photos
  */
  new cronJob('00 */10 * * * * ', function(){
     var now = moment(),
-		ftpPhotoFetcher = require('./utils/ftp-photo-fetcher'),
+    // ftpPhotoFetcher = require('./utils/ftp-photo-fetcher'),
         emailPhotoFetcher = require('./utils/email-photo-fetcher');
 
     winston.info(environment + ' Every 10 minutes, starting at ' + now.format());
 
-	if (environment === 'production'){
+  if (environment === 'production'){
     
-        winston.info(environment + ' ftpPhotoFetcher.processNewPhotos')
+        // winston.info(environment + ' ftpPhotoFetcher.processNewPhotos')
 
-        ftpPhotoFetcher.processNewPhotos(PhotoModel, function(err, results){
-            if (err) { winston.info(err); }
-            var finishedEnvironmentAt = moment();
-            winston.info(environment + 'ftpPhotoFetcher.processNewPhotos started at ' + now.format() + ', ended at ' + finishedEnvironmentAt.format() + ', duration ' + now.diff(finishedEnvironmentAt) + 'ms');
-            winston.info((results ? results.length : 0) + " images processed");
-        });
+        // ftpPhotoFetcher.processNewPhotos(PhotoModel, function(err, results){
+        //     if (err) { winston.info(err); }
+        //     var finishedEnvironmentAt = moment();
+        //     winston.info(environment + 'ftpPhotoFetcher.processNewPhotos started at ' + now.format() + ', ended at ' + finishedEnvironmentAt.format() + ', duration ' + now.diff(finishedEnvironmentAt) + 'ms');
+        //     winston.info((results ? results.length : 0) + " images processed");
+        // });
 
         winston.info(environment + ' emailPhotoFetcher.processUnreadEmails')
         
@@ -107,15 +107,15 @@ new cronJob('0 0 0 * * *', function(){
     var now = moment();
     winston.info(environment + ' Every 1 minutes, starting at ' + now.format());
 
-	
+  
     winston.info(environment + ' NotificationModel.clearPendingNotifications');
 
     NotificationModel.clearPendingNotifications({env: environment}, function(err, count){
-		if (err) { winston.info(err); }
-    	
-    	var finishedEnvironmentAt = moment();
-    	winston.info(environment + ' ModelUtils.clearPendingNotifications started at ' + now.format() + ', ended at ' + finishedEnvironmentAt.format() + ', duration ' + now.diff(finishedEnvironmentAt) + 'ms');
-    	winston.info((count || 0) + " records affected");
+    if (err) { winston.info(err); }
+      
+      var finishedEnvironmentAt = moment();
+      winston.info(environment + ' ModelUtils.clearPendingNotifications started at ' + now.format() + ', ended at ' + finishedEnvironmentAt.format() + ', duration ' + now.diff(finishedEnvironmentAt) + 'ms');
+      winston.info((count || 0) + " records affected");
     });
     
 }, null, true, "America/New_York");
@@ -127,5 +127,5 @@ winston.info('Started worker');
 
 
 setInterval(function(){
-	var x = 'break here';
+  var x = 'break here';
 }, 1000);
