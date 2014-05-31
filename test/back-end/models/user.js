67251d91148d9410d6b785475454467bd53d418a
Jack Bishop <jack.bishop1@gmail.com>
2014-05-31 18:12:09 -0400

    remove last ref to braintree

--- test/back-end/models/user.js
+++ test/back-end/models/user.js
@@ -1,6 +1,5 @@
 var mongooseConnection = require('../../../config/mongoose-connection').open('test'),
 	mongoose = require('mongoose'),
-  braintreeConfig = require('../../../config/braintree-config').setEnvironment('ci'),
   User = require('../../../models/user').model,
   should = require('should');
 
